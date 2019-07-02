3e7e5d354bca96eb46709c46970bb4b8d72da465
Mak Fof <43025563+mf3129@users.noreply.github.com>
2019-07-01 20:56:00 -0400

    Logical Operators

---
+++ Video2.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Logical Operators
+
+public class Video2 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        int numberOne = 1;
+        int numberTwo = 2;
+        string nameOne = "John";
+        string nameTwo = "Steve";
+        bool isValid = false;
+        
+        
+    }
+
+}
