af4254150d345071f77eea93db33bf4849a6acba
Mak Fof <43025563+mf3129@users.noreply.github.com>
2019-11-01 18:24:27 -0400

    Looping in C# Review

---
+++ Video3.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Video3 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    // loops
+    void Start()
+    {
+
+        // for loop
+        for(int i = 0; i <= 10 ; i++)
+        {
+            Debug.Log(i); 
+        }
+
+        // Array Structure in c#
+
+        int[] ages = new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+        string[] peopleNames = new string[] { "John", "Max", "Sam", "Manny" };
+
+        foreach(string name in peopleNames)
+        {
+            Debug.Log(name); 
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
