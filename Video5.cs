08173b1f12ed0a477d79526bb3831f32bfea11ee
Mak Fof <43025563+mf3129@users.noreply.github.com>
2020-03-29 16:11:35 -0400

    Lambda & Linq

--- Video5.cs
+++ Video5.cs
@@ -1,13 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using Systems.Linq; 
 using UnityEngine;
 
 public class Video5 : MonoBehaviour
 {
+	private string fullName = "Some Name"; 
     // Start is called before the first frame update
     void Start()
     {
-    	//Delegates example
+    	//Delegate example
     	// defining delegates and executing them
         del sumValuesDelegate = sumValues;
 
@@ -31,7 +33,41 @@ public class Video5 : MonoBehaviour
         // You can use them anywhere a delegate value is expected, that is, 
         // wherever an anonymous method can be used.
 
+        messagesDel messagesWithLambda = (x) => {
+        	x += " " + " in hierachy..."; 
+        	Debug.Log(x);
+        };
+
+        FindGameObject("Video5", messagesWithLambda); 
+        Debug.Log("FindGameObjectWithLambda(Video5)" + " " + FindGameObjectWithLambda("Video5")); 
+        Debug.Log("Property FullName: " + FullName);
+
+
+        //Linq Examples and lamdas
+        List<Player> players = new List<Player> {
+        	new Player("John", "Doe", 20, "johndoe@xbox.xom"),
+        	new Player("Steve", "Doe", 20, "stevedoe@xbox.xom"),
+        	new Player("Steve", "Doe", 40, "stevedoe@xbox.xom"),
+        	new Player("Jason", "Doe", 50, "stevedoe@xbox.xom"),
+        	new Player("Luis", "Toner", 60, "stevedoe@xbox.xom")
+        };
+
+        //linq
+        List<Player> playersWithTheAgeOfTwenty = players.Where(p => p.Age == 20).ToList();
+        int playersWithTheAgeOfTwentyCount = players.Count(p => p.Age == 20); 
+        Debug.Log("playersWithTheAgeOfTwentyCount: " + playersWithTheAgeOfTwentyCount);
+
+        Player playersWithTheAgeOfTwenty = players.FirstOrDefault(p => p.Age == 20); 
+        Debug.Log("playersWithTheAgeOfTwentyCount: " + playersWithTheAgeOfTwenty.FirstName); 
+
+        Player playerWithTheAgeOfTwentyLast = players.LastOrDefault(p => p.Age == 20);
+        Debug.Log("playerWithTheAgeOfTwenty: "+ playerWithTheAgeOfTwentyLast.FirstName);
     }
+
+public string FullName { get => fullName; set => fullName = value; }
+
+
+private string FindGameObjectWithLambda(string name) => GameObject.Find(name).name;
 
 
 // Delegate is a pointer to a method
