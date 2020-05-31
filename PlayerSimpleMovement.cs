c798e83b66b10833aa6050e1e402ef91d1f289f3
Mak Fof <43025563+mf3129@users.noreply.github.com>
2020-05-31 15:19:48 -0400

    Simple Movements Through Input

---
+++ PlayerSimpleMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerSimpleMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+         //Now moving 1 meter per second vs 1 meter per frame
+        // transform.Translate(Vector3.right * _speed * Time.deltaTime);
+        // transform.Translate(new Vector3(-1,0,0) * _speed * Time.deltaTime);
+        float HorizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(new Vector3(HorizontalInput,0,0) * _speed * Time.deltaTime);
+    }
+}
